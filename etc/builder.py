e7ba68a8203feb21843115cbd957fa204a9daebd
Edmund Warrick <Edmund.Warrick@diamond.ac.uk>
2016-09-01 09:19:38 +0000

    Implemented module - just sets/reads time constant and reads out channel X for now

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,6 @@
+from iocbuilder import AutoSubstitution
+from iocbuilder.modules.streamDevice import AutoProtocol
 
-
+class ametekLockIn(AutoSubstitution, AutoProtocol):
+    TemplateFile = 'ametekLockIn.template'
+    ProtocolFiles = ['ametekLockIn.proto']
